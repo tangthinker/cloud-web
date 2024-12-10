3237d7ddaddb0067967fa273c4798cd3382ada2e
tangyubin <tangyubin@tal.com>
2024-12-10 18:45:55 +0800

    feat: add fetch

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -13,9 +13,15 @@ function List(props) {
             {props.listdata?.map((item, index) => {
                 if (!item.Dir) {
                     if (item.Filename.endsWith(".jpg") || item.Filename.endsWith(".png")) {
+                        let result = ""
+                        const response = fetch("http://127.0.0.1:9999/api/v1/storage/get?path=" + item.Path)
+                            .then((response) => response.json())
+                            .then((data) => result = data.data)
+                        
+                        console.log("result: " + result);
                         return (
                             <Col span={4}>
-                                <ImgM  />
+                                <img src={`data:image/png;base64,${result}`} filename={item.Filename}  />
                             </Col>
                         );
                     }
